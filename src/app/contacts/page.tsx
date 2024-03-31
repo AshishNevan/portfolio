a36e3ed0dbd430db963df5a325d8981869fa75fe
Ashish Nevan <nevchris242@gmail.com>
2024-03-31 18:03:47 -0400

    Added LinkedIn to Contacts

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,12 @@
+import Card from "@/app/Card";
+
 export default function Page() {
     return (
-        <div className="min-h-screen bg-[#FFB574] p-4">
-            <p>Contacts</p>
+        <div className="min-h-screen bg-[#FFB574] flex flex-wrap items-center justify-around p-4">
+            <Card content="https://www.linkedin.com/in/ashishnevan" icon={"/portfolio/linkedin-icon.svg"}
+                  title="LinkedIn"
+                  desc="LinkedIn"/>
+
         </div>
     )
 }
